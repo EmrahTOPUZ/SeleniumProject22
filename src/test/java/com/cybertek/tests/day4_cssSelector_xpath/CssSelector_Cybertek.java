5e6a1e4b72a4a0b66d71a21b36889c75a8137760
EmrahTOPUZ <emrtpz@gmail.com>
2021-11-06 14:27:48 +0300

    start using cssSelector

--- src/test/java/com/cybertek/tests/day4_cssSelector_xpath/CssSelector_Cybertek.java
+++ src/test/java/com/cybertek/tests/day4_cssSelector_xpath/CssSelector_Cybertek.java
@@ -13,7 +13,7 @@ public class CssSelector_Cybertek {
 
         driver.get("http://practice.cybertekschool.com/forgot_password");
 
-        WebElement element = driver.findElement(By.cssSelector("a[class='nav-link']"));
+        WebElement homeLink = driver.findElement(By.cssSelector("a[class='nav-link']"));
         //WebElement element = driver.findElement(By.cssSelector("a.nav-link"));
 
         WebElement forgotPassHeader = driver.findElement(By.tagName("h2"));
@@ -26,6 +26,12 @@ public class CssSelector_Cybertek {
 
         WebElement poweredByText = driver.findElement(By.cssSelector("div[style='text-align: center;']"));
 
+        System.out.println("homeLink.isDisplayed() = " + homeLink.isDisplayed());
+        System.out.println("forgotPassHeader.isDisplayed() = " + forgotPassHeader.isDisplayed());
+        System.out.println("emailLabel.isDisplayed() = " + emailLabel.isDisplayed());
+        System.out.println("inputEmail.isDisplayed() = " + inputEmail.isDisplayed());
+        System.out.println("retrievePassWordButton.isDisplayed() = " + retrievePassWordButton.isDisplayed());
+        System.out.println("poweredByText.isDisplayed() = " + poweredByText.isDisplayed());
 
 
 
