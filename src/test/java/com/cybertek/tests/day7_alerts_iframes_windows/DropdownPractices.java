bc2049fa4c3f339bb7278b95f7869fe339ae0972
EmrahTOPUZ <emrtpz@gmail.com>
2021-11-20 00:18:06 +0300

    how to handle dropdowns

--- src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/DropdownPractices.java
@@ -1,7 +1,44 @@
 package com.cybertek.tests.day7_alerts_iframes_windows;
 
-public class TestNg {
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
+public class DropdownPractices {
+
+    WebDriver driver;
+
+
+    @BeforeClass
+    public void setupClass() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get("http://practice.cybertekschool.com/dropdown");
+    }
+    @Test
+    public void test1_simple_dropdowns_test() {
+
+        Select simpleDropdown = new Select(driver.findElement(By.xpath("//select[@id='dropdown']")));
+
+        WebElement currentlySelectedOption = simpleDropdown.getFirstSelectedOption();
+
+        String actualTextOfCurrentlyOption = currentlySelectedOption.getText();
+        String expectedText = "Please select an option";
+
+        Assert.assertTrue(actualTextOfCurrentlyOption.equals(expectedText));
+
+        //Doing everything in one line
+        //Assert.assertEquals(simpleDropdown.getFirstSelectedOption().getText(), "Please select an option");
+
+    }
 }
 
 /**
